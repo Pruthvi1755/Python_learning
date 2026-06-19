d956d41750ecf142c9137f05c9ab93e628c40966
Pruthvi T S <your-real-email@example.com>
2026-06-19 20:58:03 +0530

    Initial commit

---
+++ Machine_Learning/Pandas_Matplotlib/Pandas.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+data1 = [100, 200, 300]
+# data2 = [100.1, 200.2, 300.3]
+# data3 = ["a", "b", "c"]
+# data3 = [True, False, True]
+
+# series = pd.Series(data2) #Series "S" caps
+
+
+
+# print(series)
+
+
+display(data1)
+
