70b027ec8ac166bc0db7823ddf33a07a68545343
Pruthvi T S <your-real-email@example.com>
2026-07-01 11:08:57 +0530

    Auto Save

--- advance Python/Decorators/Employee loginTime program(Decorators)/login.py
+++ advance Python/Decorators/Employee loginTime program(Decorators)/login.py
@@ -2,5 +2,5 @@ from datetime import datetime
 def login_time(param):
     def inner_function():
         param()
-        print(param.__name__(), datetime.today)
+        print(param.__name__, datetime.today)
     return inner_function
