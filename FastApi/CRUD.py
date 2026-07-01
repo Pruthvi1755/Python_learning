36938cf215b41bed37263a203bf56a310d3ef5cc
Pruthvi T S <your-real-email@example.com>
2026-07-01 19:39:20 +0530

    Auto Save

--- FastApi/CRUD.py
+++ FastApi/CRUD.py
@@ -10,20 +10,40 @@ class Todo(BaseModel):
     title: str
     description: str
 
+# create--
 @app.post("/todos")
 def create_todo(todo: Todo):
     todos.append(todo)
     return {"message": "Todo created successfully"}
 
+# Read all--
 @app.get("/todos")
 def get_todos():
     return todos
 
+# Read one--
 @app.get("/todos/{id}")
 def get_info(id: int):
     for todo in todos:
         if todo.id == id:
             return todo
-
     return {"message": "Todo not found"}
+
+# Update--
+@app.put("/todos/{id}")
+def update(id:int, update:Todo):
+    for i,todo in enumerate(todos):
+        if todo.id == todo.id:
+            todos[i] = update
+            return{
+                'id updated'
+            }
+        
+# Delete--
+@app.delete('/todos/{id}')
+def delete(id:int):
+    for i, todo in enumerate(todos):
+        if todo.id == id:
+            todos.pop(i)
+            return {"deleted"}
 
